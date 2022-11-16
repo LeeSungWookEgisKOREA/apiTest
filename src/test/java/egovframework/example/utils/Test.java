a8a1f5321cb4222781542d8151e33e25ee367a46
lee sung wook <lsuk3165>
2022-11-16 23:34:09 +0900

    수정

--- src/test/java/egovframework/example/utils/Test.java
+++ src/test/java/egovframework/example/utils/Test.java
@@ -3,61 +3,133 @@ package egovframework.example.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.List;
 
+import javax.xml.XMLConstants;
 import javax.xml.crypto.dsig.XMLObject;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.json.JSONObject;
 import org.json.XML;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		JSONObject jso = getApiXml();
-		System.out.println(jso.toString(4));
+		
+		System.out.println(getXMl2JsonStr(getApiXml()));
+		
+		System.out.println("====================");
+		//xml 테스트
+		Document doc = convertStringToDocument(getApiXml());
+        
+        String str = convertDocumentToString(doc);
+        System.out.println(str);
 	}
-	
-	public static JSONObject getApiXml() throws IOException {
+	/**
+	 * 
+	 * @return String
+	 * @throws IOException
+	 */
+	public static String getApiXml(){
 		final String APIkey = "5a576e43676c737536394373735a4f";
 		StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088"); /*URL*/
-		urlBuilder.append("/" +  URLEncoder.encode(APIkey,"UTF-8") ); /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
-		urlBuilder.append("/" +  URLEncoder.encode("xml","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
-		urlBuilder.append("/" + URLEncoder.encode("TbLeaveOpinion","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
-		urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
-		urlBuilder.append("/" + URLEncoder.encode("5","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
-		// 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
-		
-		// 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
-		urlBuilder.append("/" + URLEncoder.encode("20220301","UTF-8")); /* 서비스별 추가 요청인자들*/
-		
-		URL url = new URL(urlBuilder.toString());
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-type", "application/xml");
-		System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
-		BufferedReader rd;
+		StringBuilder sb = null;
+		try {
+			urlBuilder.append("/" +  URLEncoder.encode(APIkey,"UTF-8") ); /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
+			urlBuilder.append("/" +  URLEncoder.encode("xml","UTF-8") ); /*요청파일타입 (xml,xmlf,xls,json) */
+			urlBuilder.append("/" + URLEncoder.encode("TbLeaveOpinion","UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
+			urlBuilder.append("/" + URLEncoder.encode("1","UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
+			urlBuilder.append("/" + URLEncoder.encode("5","UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+			// 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
+			
+			// 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
+			urlBuilder.append("/" + URLEncoder.encode("20220301","UTF-8")); /* 서비스별 추가 요청인자들*/
+			
+			URL url = new URL(urlBuilder.toString());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/xml");
+			
+			System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+			BufferedReader rd;
 
-		// 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
-		if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} else {
-				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-		}
-		StringBuilder sb = new StringBuilder();
-		String line;
-		while ((line = rd.readLine()) != null) {
-				sb.append(line);
-		}
-		rd.close();
-		conn.disconnect();
-		JSONObject json =XML.toJSONObject(sb.toString());
-		return json;
+			// 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			} else {
+					rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+			sb = new StringBuilder();
+			String line;
+			while ((line = rd.readLine()) != null) {
+					sb.append(line);
+			}
+			rd.close();
+			conn.disconnect();		
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return sb.toString();
 	}
 	public static JSONObject getXMl2JsonObject(String xml) {
-		JSONObject json = XML.toJSONObject(xml);
-        /*String jsonStr = json.toString(4);
-        System.out.println(jsonStr);*/
-		return json;
+		JSONObject jsonObj = XML.toJSONObject(xml);
+       
+		return jsonObj;
 	}
+	public static String getXMl2JsonStr(String xml) {
+		JSONObject jsonObj = XML.toJSONObject(xml);
+        String jsonStr = jsonObj.toString(4);//4 인덴트 처리
+        
+		return jsonStr;
+	}
+	
+	private static String convertDocumentToString(Document doc) {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer;
+        try {
+            transformer = tf.newTransformer();
+            // below code to remove XML declaration
+            // transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(doc), new StreamResult(writer));
+            String output = writer.getBuffer().toString();
+            return output;
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+
+    private static Document convertStringToDocument(String xmlStr) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+        DocumentBuilder builder;  
+        try  
+        {  
+            builder = factory.newDocumentBuilder();  
+            Document doc = builder.parse( new InputSource( new StringReader( xmlStr ) ) ); 
+            return doc;
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        } 
+        return null;
+    }
 }
